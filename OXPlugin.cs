2591d5c80c1603612ab626571859b39b44ac0511
Thanakorn028 <thanakorn_job@hotmail.com>
2018-05-18 19:58:13 +0700

    Update OXPlugin.cs

--- OXPlugin.cs
+++ OXPlugin.cs
@@ -542,31 +542,17 @@ namespace DNWS
 
                         if (game.Status == Game.X_WIN)
                         {
-                            TimeSpan ts =stop.Elapsed;
-                            String elapsedTime=String.Format("{0:00}:{1:00}:{2:00}.{3:00}", 
-                            ts.Hours,ts.Minuites,ts.Seconds,
-                            ts.Milliseconds/10);
-                            sb.Append("<h2>RunTime</h2>"+elapsedTime);   
+                           
                             sb.Append("<h2>X WIN!!!</h2>");
                         }
                         else if (game.Status == Game.O_WIN)
                         {
-                             TimeSpan ts =stop.Elapsed;
-                            String elapsedTime=String.Format("{0:00}:{1:00}:{2:00}.{3:00}", 
-                            ts.Hours,ts.Minuites,ts.Seconds,
-                            ts.Milliseconds/10);
-                            sb.Append("<h2>RunTime</h2>"+elapsedTime);
-                            sb.Append("<h2>Draw!!!</h2>")
+                            
                             sb.Append("<h2>O WIN!!!</h2>");
                         }
                         else if (game.Status == Game.DRAW)
                         {
-                            TimeSpan ts =stop.Elapsed;
-                            String elapsedTime=String.Format("{0:00}:{1:00}:{2:00}.{3:00}", 
-                            ts.Hours,ts.Minuites,ts.Seconds,
-                            ts.Milliseconds/10);
-                            sb.Append("<h2>RunTime</h2>"+elapsedTime);
-                            sb.Append("<h2>Draw!!!</h2>")
+                           
                             sb.Append("<h2>Draw</h2>");
                         }
                         else // Game is not finished yet
